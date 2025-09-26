65cadcb8ce9bee03d05115ac489131192540b903
Souméya Diop <soumeyadiop@gmail.com>
2025-09-26 16:51:11 -0400

    did main skeleton for Order.h, removed OrdersDriver.h because not needed

--- Order/Order.h
+++ Order/Order.h
@@ -1,7 +1,45 @@
+#pragma once
+
 namespace WarzoneOrder {
 
+    /*------------------------------------------ORDER SUPER-CLASS------------------------------------------------*/
     class Order {
 
     };
+
+    /*------------------------------------------ SINGLE ORDER SUB-CLASSES------------------------------------------------*/
+
+    class Deploy : public Order {
+
+    };
+
+    class Advance : public Order {
+
+    };
+
+    class Bomb : public Order {
+
+    };
+
+    class Blockade : public Order {
+
+    };
+
+    class Airlift : public Order {
+
+    };
+
+    class Negotiate : public Order {
+
+    };
+
+    /*------------------------------------------ORDERLIST CLASS--------------------------------------------------*/
+    class OrderList {
+
+    };
+
+    /*-------------------------------------------TEST-DRIVER FUNCTION---------------------------------------------*/
+
+    void testOrdersList();
 
 }

6103792c1157394d3f090d9a5e8ced12053484a4
Souméya Diop <soumeyadiop@gmail.com>
2025-09-26 17:11:42 -0400

    added the validate and execute methods signatures

--- Order/Order.h
+++ Order/Order.h
@@ -4,33 +4,74 @@ namespace WarzoneOrder {
 
     /*------------------------------------------ORDER SUPER-CLASS------------------------------------------------*/
     class Order {
+        
+        public:
 
+        //pure virtual functions
+        virtual bool validate() const =0;
+        virtual void execute() =0;
     };
 
     /*------------------------------------------ SINGLE ORDER SUB-CLASSES------------------------------------------------*/
 
+    //DEPLOY
     class Deploy : public Order {
+       
+        public:
 
+        //function overrides
+        bool validate() const override;
+        void execute() override;
     };
 
+    //ADVANCE
     class Advance : public Order {
 
+        public:
+
+        //function overrides
+        bool validate() const override;
+        void execute() override;
     };
 
+    //BOMN
     class Bomb : public Order {
 
+        public:
+
+        //function overrides
+        bool validate() const override;
+        void execute() override;
     };
 
+    //BLOCKADE
     class Blockade : public Order {
 
+        public:
+
+        //function overrides
+        bool validate() const override;
+        void execute() override;
     };
 
+    //AIRLIFT
     class Airlift : public Order {
 
+        public:
+
+        //function overrides
+        bool validate() const override;
+        void execute() override;
     };
 
+    //NEGOTIATE
     class Negotiate : public Order {
 
+        public:
+
+        //function overrides
+        bool validate() const override;
+        void execute() override;
     };
 
     /*------------------------------------------ORDERLIST CLASS--------------------------------------------------*/

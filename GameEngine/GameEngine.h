4f9bb555b37d08076c2025905d95df12c883766f
rimmch <rim.charafeddine@mail.concordia.ca>
2025-10-05 22:40:17 -0400

    removed pool tracker

--- GameEngine/GameEngine.h
+++ GameEngine/GameEngine.h
@@ -105,7 +105,6 @@ namespace WarzoneEngine {
         std::queue<Player*> playerQueue;  ///< Queue managing player turn order
         Player* currentPlayer = nullptr;  ///< Pointer to the currently active player
 
-        std::unordered_map<Player*, int> reinforcementPools; // Track each player's pool
 
         //-- Helper and Internal Methods --//
 
